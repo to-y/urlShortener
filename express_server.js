6f460312abbe6a68d0609d6cd6eeac8cb7914902
to-y <yitong922@gmail.com>
2016-09-16 06:18:13 +0000

    Fixed Indentations

--- express_server.js
+++ express_server.js
@@ -14,14 +14,26 @@ app.use(bodyParser.urlencoded());
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+//
 app.get('/', function(req, res) {
 
+});
+
+//
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+//gets information from /urls page, renders page and declares linked variables
+app.get("/urls", (req, res) => {
+  res.render("urls_index", {
+    urls: urlDatabase
+  });
 });
 
 //renders a new page where form exists
@@ -39,41 +51,27 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortenedURL}`);
 });
 
+//
 app.delete("/urls/:id", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect(`/urls`);
 })
 
+//
 app.put("/urls/:id", (req, res) => {
   let changes = req.body.changedURL;
+
   if (urlDatabase.hasOwnProperty(req.params.id)) {
     urlDatabase[req.params.id] = changes;
     res.redirect("/urls");
-    };
-
+  };
 })
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-// <!-- <%= link_to(shortenLink,'/urls/new') %> -->
-
-//gets information from /urls page, renders page and declares linked variables
-app.get("/urls", (req, res) => {
-
-  res.render("urls_index", {
-    urls: urlDatabase
-      });
-});
 
 //:id is essentially whatever the user decides to put behind /urls/
 //req.params.id means whatever is in the request url. parameters of these requests and id refers to whatever behind the :
 //req.query would be such as /urls?:whatever
 //then renders the parameters from the ID and displays it in urls_show as shortURL in urls_show
-
 app.get("/urls/:id", (req, res) => {
-
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id]
@@ -82,9 +80,10 @@ app.get("/urls/:id", (req, res) => {
 
 });
 
-
+//
 app.get("/:shortURL", (req, res) => {
   let fullURL = urlDatabase[req.params.shortURL];
+
   console.log(urlDatabase);
   if (fullURL === undefined) {
     // res.redirect("/urls/go back page");
@@ -94,17 +93,21 @@ app.get("/:shortURL", (req, res) => {
   }
 });
 
+//
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//
 function generateRandomString() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < 6; i++ )
+    for(let i = 0; i < 6; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
-
 }
+
+
+// <!-- <%= link_to(shortenLink,'/urls/new') %> -->
