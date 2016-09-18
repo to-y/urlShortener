b0d46ff5bbc5a59204be01db52c19218c60cb89e
to-y <yitong922@gmail.com>
2016-09-18 04:12:56 +0000

    added 404 page, moved all functions to mongo and altered server to call functions from module

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const mongo = require('./mongo_connect.js')
 const express = require('express');
 const app = express();
 const helpers = require('express-helpers')(app);
@@ -9,88 +10,103 @@ const methodOverride = require('express-method-override')
 // default port 8080
 const PORT = process.env.PORT || 8080;
 
-
+//
 app.use(bodyParser.urlencoded());
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-};
 
-//
+//redirects to localhost:8080/urls
 app.get('/', function(req, res) {
-
+  res.redirect("/urls");
 });
 
-//
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-//gets information from /urls page, renders page and declares linked variables
+//gets information from /urls page
+//calls all function and inputs collection name to find database
+//renders page and links variables from database to to urls_index page
 app.get("/urls", (req, res) => {
-  res.render("urls_index", {
-    urls: urlDatabase
+  mongo.all((urls) => {
+    res.render("urls_index", {
+      urls: urls
+    })
   });
 });
 
-//renders a new page where form exists
+//renders 404 page
+app.get("/urls/404", (req, res) => {
+  mongo.all((urls) => {
+    res.render("urls_404", {
+      urls: urls
+    })
+  });
+});
+
+//renders a new page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 //post takes the Method POST from the ejs file and then reads the form action to determine which app.post to use
+//make random string into variable so all random strings are same
+//insert by getting URL from user input by taking it out of req.body, and create new short url
+//two parameters inserted into insert function to create object
+//redirects to a new url/:id page
 app.post("/urls", (req, res) => {
   let shortenedURL = generateRandomString();
-  // debug statement to see POST parameters
-  console.log(req.body);
-  urlDatabase[shortenedURL] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortenedURL}`);
+  mongo.insert({shortURL: shortenedURL, longURL: req.body.longURL}, (err) => {
+    if (err) return res.redirect("urls/new");
+    res.redirect(`/urls/${shortenedURL}`);
+  })
 });
 
-//
+//deletes using id call from urls/:id => inputted by user on address bar
+//so request parameters named id
+//redirects to homepage even if error
 app.delete("/urls/:id", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect(`/urls`);
+  mongo.delete(req.params.id, (err) => {
+    res.redirect("/urls");
+  })
 })
 
-//
+//put because method is called on post
+//takes user input declared id to find object in mongo
+//takes user input from html page (req.body) which is declared in html page as changedURL
+//redirect to home page
 app.put("/urls/:id", (req, res) => {
   let changes = req.body.changedURL;
-
-  if (urlDatabase.hasOwnProperty(req.params.id)) {
-    urlDatabase[req.params.id] = changes;
+  mongo.update(req.params.id, changes, (err) => {
+    if (err) return (err);
     res.redirect("/urls");
-  };
-})
+  })
+  });
 
 //:id is essentially whatever the user decides to put behind /urls/
 //req.params.id means whatever is in the request url. parameters of these requests and id refers to whatever behind the :
 //req.query would be such as /urls?:whatever
 //then renders the parameters from the ID and displays it in urls_show as shortURL in urls_show
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
-  };
-  res.render("urls_show", templateVars);
+  mongo.get(req.params.id, (err, urlData) => {
+    let shortURL = req.params.id;
+    if (err) return (err);
+    res.render("urls_show", {
+      shortURL: shortURL,
+      longURL: urlData.longURL
+    })
+  })
+})
 
-});
-
-//
+//takes short url id inputted by user into address bar and sticks it into get in mongo
+//redirects to 404 if short url does not exist
+//else redirect status and goes to the longURL found in urlData object from database
 app.get("/:shortURL", (req, res) => {
-  let fullURL = urlDatabase[req.params.shortURL];
-
-  console.log(urlDatabase);
-  if (fullURL === undefined) {
-    // res.redirect("/urls/go back page");
-    console.log("error");
-  } else {
-  res.status(301).redirect(fullURL);
+  let short = req.params.shortURL;
+  mongo.get(short, (err, urlData) => {
+    if (!urlData) {
+      res.status(404).redirect("/urls/404");
+    } else {
+    res.status(301).redirect(urlData.longURL);
   }
+  })
 });
 
 //
@@ -98,16 +114,12 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-//
+//generates random string of 6 length given A-Z, a-z, 0-9
 function generateRandomString() {
     let text = "";
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for(let i = 0; i < 6; i++)
+    for (let i = 0; i < 6; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
-
     return text;
 }
-
-
-// <!-- <%= link_to(shortenLink,'/urls/new') %> -->
---
+++ mongo_connect.js
@@ -0,0 +1,81 @@
+"use strict";
+
+//import mongo
+const MongoClient = require("mongodb").MongoClient;
+//define loopback to connect to and name of the database
+const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
+//define name of collection within database
+const collection_name = "urls";
+
+
+module.exports = {
+
+  //function to run connection to mongo via ip and callback function
+  db: function(cb) {
+    MongoClient.connect(MONGODB_URI, cb);
+
+  },
+
+  //function to find all objects in defined collection and put it to an array
+  //@params collection name
+  //@returns objects within collection
+  all: function(cb) {
+    //calls db function within module exports to connect to databse
+    this.db(function(err, db) {
+      //define collection name, find all objects, push to array
+      db.collection(collection_name).find().toArray(function(err, data) {
+        if (err) return cb(err);
+        //return data into callback
+        cb(data);
+      })
+    });
+  },
+
+  //function to get one object from collection in database
+  //@params single shortened url name
+  //@returns object which short url was found in
+  get: function(url, cb) {
+    this.db(function(err, db) {
+      if (err) return cb(err);
+      db.collection(collection_name).findOne({"shortURL": url, }, function(err, data) {
+        if (err) return cb(err);
+        if (data === null) return cb(new Error('Not Found'));
+        cb(null, data);
+      })
+    });
+  },
+
+  //function to update one object within collection
+  //@params short url to find the object, new long url to replace
+  //@replaces old long url address with new url address
+  update: function(url, newLongURL, cb) {
+    this.db(function(err, db) {
+      if (err) return cb(err);
+      db.collection(collection_name).updateOne({"shortURL": url}, { $set: {"longURL" : newLongURL }}, function(err) {
+        if (err) return cb(err);
+        cb(null);
+      })
+    });
+  },
+
+  //function to insert new long url into collection
+  //@params new long url name
+  //@adds new object into collection
+  insert: function(newLongURL, cb) {
+    this.db(function(err, db) {
+      if (err) return cb(err);
+      db.collection(collection_name).insert(newLongURL, cb);
+    });
+  },
+
+  //function that deletes object from the collection
+  //@params finds object based on given short URL
+  //@returns removal of object from the database
+  delete: function(url, cb) {
+    this.db(function(err, db) {
+      db.collection(collection_name).deleteOne({"shortURL": url}, cb);
+    });
+  }
+}
+
+
